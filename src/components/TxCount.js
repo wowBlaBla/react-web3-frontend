0aba5f83c425ee9c5dc19503f92bb12bd1ef7e8b
wowBlaBla <wow1992122@gmail.com>
2022-04-05 09:27:46 +0900

    update layout of TxCount Component

--- src/components/TxCount.js
+++ src/components/TxCount.js
@@ -5,16 +5,16 @@ const TxCount = (props) => {
 	return (
 		<div className='mt-5'>
 			<h2 className='text-center'>The number of total fund transactions:</h2>
-				<div className='row justify-content-center'>
-					<div className=' form-group col-4'>
-						<input className='form-control'
-										type="text"
-										value={ props.tokenAddr }
-										onChange={(e) => props.setTokenAddr(e.target.value)}
-										placeholder="Input token address. Ex: 0x..."
-						/>
-					</div>
+			<div className='row justify-content-center'>
+				<div className=' form-group col-4'>
+					<input className='form-control'
+									type="text"
+									value={ props.tokenAddr }
+									onChange={(e) => props.setTokenAddr(e.target.value)}
+									placeholder="Input token address. Ex: 0x..."
+					/>
 				</div>
+			</div>
 			<h4 className='text-center text-primary mt-2'>{ props.txCount }</h4>
 		</div>
 	)
